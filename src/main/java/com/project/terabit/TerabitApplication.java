ad13db9197e992369522ae990794de56df92377a
AtonyCallistusJ <acallistus@gmail.com>
2019-07-20 12:32:16 +0530

    initial commit

---
+++ src/main/java/com/project/terabit/TerabitApplication.java
@@ -0,0 +1,13 @@
+package com.project.terabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerabitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/terabit/TerabitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.terabit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TerabitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
